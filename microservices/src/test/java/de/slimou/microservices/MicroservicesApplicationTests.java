74a53fb175ff055c0b3a0920f98ef56ade1b20a8
Salim Oussayfi <box@slimou.net>
2021-02-18 19:56:34 +0100

    added modules

---
+++ microservices/src/test/java/de/slimou/microservices/MicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.slimou.microservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
