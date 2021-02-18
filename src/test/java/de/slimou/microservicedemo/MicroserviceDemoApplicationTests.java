eb5bbb6defde64e63d94b479372746b4350a811c
Salim Oussayfi <box@slimou.net>
2021-02-18 19:47:21 +0100

    initial project

---
+++ src/test/java/de/slimou/microservicedemo/MicroserviceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.slimou.microservicedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
